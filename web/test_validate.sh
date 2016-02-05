a2a130d32fa89d912650222657f475f03ec4badd
davidrichardson <david.m.richardson@gmail.com>
2016-02-05 10:24:36 +0000

    correct curl syntax

--- web/test_validate.sh
+++ web/test_validate.sh
@@ -3,4 +3,4 @@ pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd`
 popd > /dev/null
 
-curl -X POST -F "format=json;rule_set_name=test_rules;metadata_file=$SCRIPTPATH/../t/data/test_data.json" http://127.0.0.1:3000/validate_upload
+curl -X POST -Fformat=json -Frule_set_name=test_rules -Fmetadata_file=@$SCRIPTPATH/../t/data/test_data.json http://127.0.0.1:3000/validate_metadata
