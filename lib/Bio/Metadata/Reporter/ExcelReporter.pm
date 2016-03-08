504c0b081036a6338862bb6555639de60f49523d
davidrichardson <david.m.richardson@gmail.com>
2016-03-08 10:52:33 +0000

    correct column border placement

--- lib/Bio/Metadata/Reporter/ExcelReporter.pm
+++ lib/Bio/Metadata/Reporter/ExcelReporter.pm
@@ -72,6 +72,8 @@ sub report {
         my $col_to_cover_per_attr = 1;
         $col_to_cover_per_attr++ if ( $ac->use_units );
         $col_to_cover_per_attr++ if ( $ac->use_uri );
+        $col_to_cover_per_attr++ if ( $ac->use_source_ref );
+        $col_to_cover_per_attr++ if ( $ac->use_id );
         #$col_to_cover_per_attr =+ 2 if ( $ac->use_ref_id );
 
         $col = $col + ( $ac->max_count * $col_to_cover_per_attr );
