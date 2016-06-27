3ee26e8d491bb26cb933999eba14f87f46476a57
davidrichardson <david.m.richardson@gmail.com>
2016-06-27 11:08:59 +0100

    Require specific version of JSON::Validator for error message format

--- lib/Bio/Metadata/ValidateSchema/EntityValidator.pm
+++ lib/Bio/Metadata/ValidateSchema/EntityValidator.pm
@@ -222,14 +222,13 @@ sub prepare_warnings {
 
 	if ($w->path=~/SELECTOR/) {
 		foreach my $msg (@a) {
-			next if $msg=~/^oneOf failed\:/;
+			next if $msg=~/oneOf failed\:\s\(/;
 			my $branch= $1 if $msg=~/branch\:(\d+)\]/;
 			next if $branch && exists $notbranch{$branch};
 			$seen=1;
 			$msg=~s/^\s\///;
 			$msg=~s/\[\w+\sbranch\:\d+\][\s|\)]//;
 			my $name=$1 if $msg=~/^(\w+)\:\s.+/;
-      print STDERR $msg.$/;
 			die("[ERROR] Attribute was not found in '$msg'") if !$name;
 			push @warnings,&E($name,$msg);
 		}
