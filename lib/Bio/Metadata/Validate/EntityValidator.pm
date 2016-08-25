515341b6549435243b5a7e117c0688d2c8e7bb0b
Peter Harrison <peter@ebi.ac.uk>
2016-08-25 13:31:36 +0100

    Fix line ending

--- lib/Bio/Metadata/Validate/EntityValidator.pm
+++ lib/Bio/Metadata/Validate/EntityValidator.pm
@@ -42,7 +42,7 @@ use Bio::Metadata::Validate::DateAttributeValidator;
 use Bio::Metadata::Validate::RelationshipValidator;
 use Bio::Metadata::Validate::NcbiTaxonomyValidator;
 use Bio::Metadata::Validate::OntologyAttrNameValidator;
-use Bio::Metadata::Validate::SubmissionsIdentifierValidator
+use Bio::Metadata::Validate::SubmissionsIdentifierValidator;
 use Bio::Metadata::Faang::FaangBreedValidator;
 
 
