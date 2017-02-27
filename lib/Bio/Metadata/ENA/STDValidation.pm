7a05b38ec134589d3df84cc70b70de2794648704
Peter Harrison <peter@ebi.ac.uk>
2017-02-27 15:39:58 +0000

    Add STD validation module

--- lib/Bio/Metadata/ENA/STDValidation.pm
+++ lib/Bio/Metadata/ENA/STDValidation.pm
@@ -20,5 +20,83 @@ use Bio::Metadata::Rules::Rule;
 use Bio::Metadata::Validate::ValidationOutcome;
 use Bio::Metadata::Validate::EntityValidator;
 
+
+has 'study_rule_set' => (
+  is      => 'ro',
+  isa     => 'Bio::Metadata::Rules::RuleSet',
+  coerce  => 1,
+  default => sub {
+    {
+      name        => 'Study XML section - study',
+      rule_groups => [
+        {
+          name  => 'Study rules',
+          rules => [
+            {
+              name      => 'study_alias',
+              mandatory => 'mandatory',
+              type      => 'text'
+            },
+            {
+              name      => 'center_name',
+              mandatory => 'mandatory',
+              type      => 'text'
+            },
+            {
+              name      => 'STUDY_TITLE',
+              mandatory => 'mandatory',
+              type      => 'text'
+            },
+            {
+              name      => 'STUDY_TYPE',
+              mandatory => 'mandatory',
+              type      => 'text'
+            },
+            {
+              name      => 'STUDY_DESCRIPTION',
+              mandatory => 'optional',
+              type      => 'text'
+            },
+          ]
+        },
+      ]
+    };
+  }
+);
+
+sub validate_std {
+  my ( $self, $std_entries ) = @_;
+
+  my %blocks;
+  for my $std_entity (@$std_entries) {
+    $blocks{ $std_entity->id } //= [];
+    push @{ $blocks{ $std_entity->id } }, $std_entity;
+  }
+
+  my @errors;
+  push @errors,
+    $self->validate_section( \%blocks, 'study',
+    $self->study_rule_set, 1, 0 );
+
+  return \@errors;
+}
+
+sub validate_section {
+  my ( $self, $blocks, $block_name, $rule_set, $just_one, $at_least_one ) = @_;
+
+  my $entities = $blocks->{$block_name} || [];
+
+  my $v =
+    Bio::Metadata::Validate::EntityValidator->new( rule_set => $rule_set );
+
+  my @errors;
+  for my $e (@$entities) {
+    my ( $outcome_overall, $validation_outcomes ) = $v->check($e);
+    push @errors, grep { $_->outcome ne 'pass' } @$validation_outcomes;
+  }
+
+  return @errors;
+}
+
 __PACKAGE__->meta->make_immutable;
 1;
