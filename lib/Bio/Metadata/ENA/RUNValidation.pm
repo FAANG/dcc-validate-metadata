be1637452de9dc244272cadebe798f46fca8073a
Jun Fan <junf@ebi.ac.uk>
2018-11-15 09:26:28 +0000

    add check on filetype_pair and checksum_method_pair in RUN

--- lib/Bio/Metadata/ENA/RUNValidation.pm
+++ lib/Bio/Metadata/ENA/RUNValidation.pm
@@ -166,12 +166,20 @@ sub checkLimitedValues(){
   foreach my $attr(@{$entity->attributes}){
     my $value = $attr->value;
     if($attr->name eq "checksum_method"){
-      unless ($attr->value eq "MD5" || $attr->value eq "SHA-256"){
+      unless ($value eq "MD5" || $value eq "SHA-256"){
         push @errorMsgs,"Wrong checksum_method value $value, which can only be either MD5 or SHA-256 according to ENA rule";
       }
     }
     if($attr->name eq "filetype"){
-      push @errorMsgs, "Wrong filetype value $value, only could be one of @filetypes" unless (exists $filetypes{$attr->value});
+      push @errorMsgs, "Wrong filetype value $value, only could be one of @filetypes" unless (exists $filetypes{$value});
+    }
+    if($attr->name eq "checksum_method_pair"){
+      unless ($value eq "MD5" || $value eq "SHA-256" || $value eq ""){
+        push @errorMsgs,"Wrong checksum_method_pair value $value, which can only be either MD5 or SHA-256 according to ENA rule";
+      }
+    }
+    if($attr->name eq "filetype_pair"){
+      push @errorMsgs, "Wrong filetype_pair value $value, only could be one of @filetypes" unless (exists $filetypes{$value} || $value eq "");
     }
   }
   return @errorMsgs;
