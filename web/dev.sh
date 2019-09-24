dfa4c2a3a045964e629200b61fcad26ea0457140
Alexey Sokolov <alexey@ebi.ac.uk>
2019-09-24 12:20:52 +0100

    Initial commit

--- web/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd`
-popd > /dev/null
-
-APPLIBPATH=$SCRIPTPATH/../lib
-CARTON_PATH=$SCRIPTPATH/../local/lib/perl5/
-PERL5LIB=$APPLIBPATH:$CARTON_PATH:${PERL5LIB}
-morbo -w $SCRIPTPATH/validate_metadata.conf -w $SCRIPTPATH/public -w $SCRIPTPATH/templates -w $APPLIBPATH -w $CARTON_PATH $SCRIPTPATH/validate_metadata.pl 
--- web/perf.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd`
-popd > /dev/null
-
-APPLIBPATH=$SCRIPTPATH/../lib
-CARTON_PATH=$SCRIPTPATH/../local/lib/perl5/
-PERL5LIB=$APPLIBPATH:$CARTON_PATH:${PERL5LIB}
-perl -d:NYTProf $SCRIPTPATH/validate_metadata.pl daemon
