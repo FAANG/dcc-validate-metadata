f20d95db1a634dfa2ad0536049e5e08e6d28ba94
Alexey Sokolov <alexey@ebi.ac.uk>
2019-09-27 16:50:09 +0100

    Adding new items to gitignore and new todo item

--- metadata_validation_conversion/conversion/tasks.py
+++ metadata_validation_conversion/conversion/tasks.py
@@ -15,6 +15,7 @@ def read_excel_file():
     headers = [convert_to_snake_case(item) for item in sh.row_values(0)]
     field_names_indexes = get_field_names_and_indexes(headers)
     for row_number in range(1, sh.nrows):
+        # TODO should read sheet name and decide which json to use
         data.append(get_organism_data(sh.row_values(row_number),
                                       field_names_indexes))
     print(json.dumps(data))
