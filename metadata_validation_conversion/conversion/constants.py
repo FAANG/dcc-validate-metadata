71439bde422d2e322093f79f69ea83eedba27942
Alexey Sokolov <alexey@ebi.ac.uk>
2019-09-30 16:18:42 +0100

    Adding handlers for other types

--- metadata_validation_conversion/conversion/constants.py
+++ metadata_validation_conversion/conversion/constants.py
@@ -2,7 +2,32 @@ BASE_URL = "https://raw.githubusercontent.com/FAANG/dcc-metadata/" \
            "switch_to_json-schema/json_schema/"
 ORGANISM_URL = f"{BASE_URL}type/samples/" \
                f"faang_samples_organism.metadata_rules.json"
+SPECIMEN_FROM_ORGANISM_URL = f"{BASE_URL}type/samples/" \
+                             f"faang_samples_specimen.metadata_rules.json"
+POOL_OF_SPECIMENS_URL = f"{BASE_URL}type/samples/" \
+                        f"faang_samples_pool_of_specimens.metadata_rules.json"
+CELL_SPECIMEN_URL = f"{BASE_URL}type/samples/" \
+                    f"faang_samples_purified_cells.metadata_rules.json"
+CELL_CULTURE_URL = f"{BASE_URL}type/samples/" \
+                   f"faang_samples_cell_culture.metadata_rules.json"
+CELL_LINE_URL = f"{BASE_URL}type/samples/" \
+                f"faang_samples_cell_line.metadata_rules.json"
+
+ALLOWED_SHEET_NAMES = {
+    'organism': ORGANISM_URL,
+    'specimen from organism': SPECIMEN_FROM_ORGANISM_URL,
+    'pool of specimens': POOL_OF_SPECIMENS_URL,
+    'cell specimen': CELL_SPECIMEN_URL,
+    'cell culture': CELL_CULTURE_URL,
+    'cell line': CELL_LINE_URL
+}
 
 SKIP_PROPERTIES = ['describedBy', 'schema_version', 'samples_core']
 
 SPECIAL_PROPERTIES = ['sample_name', 'unit', 'term_source_id']
+
+JSON_TYPES = {
+    'core': 'samples_core',
+    'type': None,
+    'custom': 'custom'
+}
--- metadata_validation_conversion/conversion/tasks.py
+++ metadata_validation_conversion/conversion/tasks.py
@@ -1,7 +1,9 @@
 import xlrd
 import json
 
-from .helpers import get_field_names_and_indexes, get_organism_data, convert_to_snake_case
+from .helpers import get_field_names_and_indexes, get_organism_data, \
+    convert_to_snake_case
+from .constants import ALLOWED_SHEET_NAMES
 from metadata_validation_conversion.celery import app
 
 
@@ -10,13 +12,19 @@ def read_excel_file():
     wb = xlrd.open_workbook('/Users/alexey/ebi_projects/dcc-validate-metadata/'
                             'metadata_validation_conversion/conversion/'
                             'organism.xlsx')
-    sh = wb.sheet_by_index(0)
-    data = list()
-    headers = [convert_to_snake_case(item) for item in sh.row_values(0)]
-    field_names_indexes = get_field_names_and_indexes(headers)
-    for row_number in range(1, sh.nrows):
-        # TODO should read sheet name and decide which json to use
-        data.append(get_organism_data(sh.row_values(row_number),
-                                      field_names_indexes))
-    print(json.dumps(data))
+    data = dict()
+    for sh in wb.sheets():
+        if sh.name not in ALLOWED_SHEET_NAMES:
+            print(f"There are no rules for {sh.name} type!!!")
+            return 'Failure!'
+        else:
+            tmp = list()
+            headers = [convert_to_snake_case(item) for item in sh.row_values(0)]
+            field_names_indexes = get_field_names_and_indexes(
+                headers, ALLOWED_SHEET_NAMES[sh.name])
+            for row_number in range(1, sh.nrows):
+                tmp.append(get_organism_data(sh.row_values(row_number),
+                                             field_names_indexes))
+            data[convert_to_snake_case(sh.name)] = tmp
+    print(json.dumps(data['cell_line']))
     return 'Success!'
