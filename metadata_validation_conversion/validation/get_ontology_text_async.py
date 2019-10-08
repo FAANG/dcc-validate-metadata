da7f9a5600c9100ed05f30217a4f32981227b9f3
Alexey Sokolov <alexey@ebi.ac.uk>
2019-10-08 10:59:30 +0100

    Adding async calls for ols

---
+++ metadata_validation_conversion/validation/get_ontology_text_async.py
@@ -0,0 +1,83 @@
+import requests
+import aiohttp
+import asyncio
+
+
+def parse_record(record):
+    """
+    This function will fetch term_ids from records
+    :param record: record to parse
+    :return: term_id
+    """
+    if isinstance(record, dict):
+        if 'text' in record and 'term' in record:
+            return record['term']
+    elif isinstance(record, list):
+        for sub_record in record:
+            if 'text' in sub_record and 'term' in sub_record:
+                return sub_record['term']
+
+
+def collect_ids(records):
+    """
+    Main function that will collect all ids and start ids fetching
+    :param records: records to fetch
+    :return: dict with term_ids as keys and ols results as values
+    """
+    ids = set()
+    for record in records:
+        for _, value in record['samples_core'].items():
+            ids.add(parse_record(value))
+        for _, value in record.items():
+            ids.add(parse_record(value))
+        for _, value in record['custom'].items():
+            ids.add(parse_record(value))
+    results = fetch_text_for_ids(ids)
+    return results
+
+
+def fetch_text_for_ids(ids):
+    """
+    This function will start async calls to OLS to get results for term_ids
+    :param ids: ids to call
+    :return: dict with term_ids as keys and ols results as values
+    """
+    results = dict()
+    asyncio.get_event_loop().run_until_complete(fetch_all_terms(ids, results))
+    # Not all ids can get through OLS because of bandwidth, so do sync calls
+    if len(results) < len(ids):
+        for my_id in ids:
+            if my_id not in results:
+                results[my_id] = requests.get(
+                    f"http://www.ebi.ac.uk/ols/api/search?q={my_id}"
+                ).json()['response']['docs']
+    return results
+
+
+async def fetch_all_terms(ids, results_to_return):
+    """
+    This function will create tasks for ols calls
+    :param ids: ids to fetch from ols
+    :param results_to_return: holder for results
+    """
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for my_id in ids:
+            task = asyncio.ensure_future(fetch_term(session, my_id,
+                                                    results_to_return))
+            tasks.append(task)
+        await asyncio.gather(*tasks, return_exceptions=True)
+
+
+async def fetch_term(session, my_id, results_to_return):
+    """
+    This functino will create task to call my_id from OLS
+    :param session: session to work with
+    :param my_id: term_id to check
+    :param results_to_return: json structure to parse
+    """
+    url = f"http://www.ebi.ac.uk/ols/api/search?q={my_id}"
+    async with session.get(url) as response:
+        results = await response.json()
+        if results and 'response' in results and 'docs' in results['response']:
+            results_to_return[my_id] = results['response']['docs']
