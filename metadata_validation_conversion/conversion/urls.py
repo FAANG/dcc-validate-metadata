f5e24209c7f9942d7de09ed3ad4bd0f39ef1e659
Alexey Sokolov <alexey@ebi.ac.uk>
2019-10-22 12:21:52 +0100

    Adding file upload through celery task

--- metadata_validation_conversion/conversion/urls.py
+++ metadata_validation_conversion/conversion/urls.py
@@ -5,6 +5,5 @@ app_name = 'conversion'
 urlpatterns = [
     path('', views.index, name='index'),
     path('samples', views.convert_samples, name='convert_samples'),
-    path('experiments', views.convert_experiments, name='convert_experiments'),
-    path('file_upload', views.upload_file, name='upload_file')
+    path('experiments', views.convert_experiments, name='convert_experiments')
 ]
--- metadata_validation_conversion/conversion/views.py
+++ metadata_validation_conversion/conversion/views.py
@@ -8,20 +8,19 @@ def index(request):
     return render(request, 'conversion/index.html')
 
 
+@csrf_exempt
 def convert_samples(request):
-    res = read_excel_file.apply_async(('samples',), queue='conversion')
-    return render(request, 'conversion/conversion.html', {'task_id': res.id})
-
-
-def convert_experiments(request):
-    read_excel_file.delay('experiments')
-    return HttpResponse("This is conversion app for experiments!")
+    if request.method == 'POST':
+        with open('file.xlsx', 'wb+') as destination:
+            for chunk in request.FILES['file'].chunks():
+                destination.write(chunk)
+        res = read_excel_file.apply_async(('samples', 'file.xlsx'),
+                                          queue='conversion')
+        return HttpResponse(f"Conversion started: {res.id}")
+    return HttpResponse("Please use POST method for conversion!")
 
 
 @csrf_exempt
-def upload_file(request):
-    if request.method == 'POST':
-        with open("file.xlsx", 'wb+') as destination:
-            for chunk in request.FILES['file'].chunks():
-                destination.write(chunk)
-    return HttpResponse("Uploading excel file")
+def convert_experiments(request):
+    read_excel_file.delay('experiments')
+    return HttpResponse("This is conversion app for experiments!")
