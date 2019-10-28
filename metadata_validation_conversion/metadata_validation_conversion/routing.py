1f8d8120209cf21de8dab43a0274553ca5866084
Alexey Sokolov <alexey@ebi.ac.uk>
2019-10-28 14:19:30 +0000

    Adding channels

--- metadata_validation_conversion/metadata_validation_conversion/routing.py
+++ metadata_validation_conversion/metadata_validation_conversion/routing.py
@@ -1,5 +1,12 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import ws.routing
 
 application = ProtocolTypeRouter({
-
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            ws.routing.websocket_urlpatterns
+        )
+    ),
 })
---
+++ metadata_validation_conversion/ws/routing.py
@@ -0,0 +1,7 @@
+from django.urls import re_path
+
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/submission/(?P<task_id>\w+)/$', consumers.SubmissionConsumer),
+]
