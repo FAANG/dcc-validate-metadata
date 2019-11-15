15917a0fa5ab481ced338148c060c4842eb8636a
Alexey Sokolov <alexey@ebi.ac.uk>
2019-11-15 09:11:48 +0000

    Fixing JoindedResults class to accept variable number of arguments

--- metadata_validation_conversion/validation/JoinedResults.py
+++ metadata_validation_conversion/validation/JoinedResults.py
@@ -7,34 +7,29 @@ class JoinedResults:
 
     def join_results(self):
         joined_results = dict()
+        number_of_results = len(self.results)
         for record_type in self.results[0]:
             joined_results.setdefault(record_type, list())
             for index, first_record in enumerate(self.results[0][record_type]):
-                second_record = self.results[1][record_type][index]
-                third_record = self.results[2][record_type][index]
+                records = list()
+                for i in range(0, number_of_results):
+                    records.append(self.results[i][record_type][index])
                 tmp = get_validation_results_structure(first_record['name'])
-                tmp = self.join_issues(tmp, first_record, second_record,
-                                       third_record)
+                tmp = self.join_issues(tmp, *records)
                 joined_results[record_type].append(tmp)
         return joined_results
 
     @staticmethod
-    def join_issues(to_join_to, first_record, second_record, third_record):
+    def join_issues(to_join_to, *records):
         """
-        This function will join all issues from first and second record into one
-        place
+        This function will join all issues into one result structure
         :param to_join_to: holder that will store merged issues
-        :param first_record: first record to get issues from
-        :param second_record: second record to get issues from
-        :param third_record: third record to get issues from
+        :param records: list of records to join
         :return: merged results
         """
         for issue_type in ['core', 'type', 'custom']:
             for issue in ['errors', 'warnings']:
-                to_join_to[issue_type][issue].extend(
-                    first_record[issue_type][issue])
-                to_join_to[issue_type][issue].extend(
-                    second_record[issue_type][issue])
-                to_join_to[issue_type][issue].extend(
-                    third_record[issue_type][issue])
+                for record in records:
+                    to_join_to[issue_type][issue].extend(
+                        record[issue_type][issue])
         return to_join_to
