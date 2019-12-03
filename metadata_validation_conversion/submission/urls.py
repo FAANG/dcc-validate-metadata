18a5e1e32c4dc298750a349e6d50c2a18a103078
Alexey Sokolov <alexey@ebi.ac.uk>
2019-12-03 09:41:13 +0000

    Adding file download response to subnission views

--- metadata_validation_conversion/submission/urls.py
+++ metadata_validation_conversion/submission/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 app_name = 'submission'
 urlpatterns = [
-    path('samples', views.samples_submission, name='samples_submission'),
-    path('experiments', views.experiments_submission,
+    path('samples/<str:task_id>', views.samples_submission,
+         name='samples_submission'),
+    path('experiments/<str:task_id>', views.experiments_submission,
          name='experiments_submission'),
-    path('analyses', views.analyses_submission, name='analyses_submission')
+    path('analyses/<str:task_id>', views.analyses_submission,
+         name='analyses_submission')
 ]
--- metadata_validation_conversion/submission/views.py
+++ metadata_validation_conversion/submission/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from metadata_validation_conversion.celery import app
 
-def samples_submission(request):
-    pass
 
-def experiments_submission(request):
-    pass
+def samples_submission(request, task_id):
+    validation_result = app.AsyncResult(task_id)
+    json_to_send = validation_result.get()
+    response = HttpResponse(json.dumps(json_to_send),
+                            content_type='application/json')
+    response['Content-Disposition'] = 'attachment; filename="samples.json"'
+    return response
 
-def analyses_submission(request):
-    pass
+
+def experiments_submission(request, task_id):
+    validation_result = app.AsyncResult(task_id)
+    json_to_send = validation_result.get()
+    response = HttpResponse(json.dumps(json_to_send),
+                            content_type='application/json')
+    response['Content-Disposition'] = 'attachment; filename="experiments.json"'
+    return response
+
+
+def analyses_submission(request, task_id):
+    validation_result = app.AsyncResult(task_id)
+    json_to_send = validation_result.get()
+    response = HttpResponse(json.dumps(json_to_send),
+                            content_type='application/json')
+    response['Content-Disposition'] = 'attachment; filename="analyses.json"'
+    return response
