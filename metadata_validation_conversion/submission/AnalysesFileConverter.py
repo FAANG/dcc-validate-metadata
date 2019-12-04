dc2cc8773af1523136b8f55c97dcc4a2e0269e1d
Alexey Sokolov <alexey@ebi.ac.uk>
2019-12-04 13:46:00 +0000

    Working on analysis file converter

--- metadata_validation_conversion/submission/AnalysesFileConverter.py
+++ metadata_validation_conversion/submission/AnalysesFileConverter.py
@@ -3,4 +3,28 @@ class AnalysesFileConverter:
         self.json_to_convert = json_to_convert
 
     def start_conversion(self):
-        return ''
+        analysis_xml = self.generate_analysis_xml()
+        submission_xml = self.generate_submission_xml()
+        return analysis_xml, submission_xml
+
+    @staticmethod
+    def generate_analysis_xml():
+        result = '<?xml version="1.0" encoding="UTF-8"?>\n'
+        result += '<SUBMISSION_SET>\n\t<SUBMISSION alias=\"analysis\">\n'
+        result += '\t\t<ACTIONS>\n\t\t\t<ACTION>\n\t\t\t\t<ADD/>\n\t\t\t' \
+                  '</ACTION>\n'
+        result += '\t\t\t<ACTION>\n\t\t\t\t<RELEASE/>\n\t\t\t</ACTION>\n\t\t' \
+                  '</ACTIONS>\n'
+        result += '\t</SUBMISSION>\n</SUBMISSION_SET>\n'
+        return result
+
+    @staticmethod
+    def generate_submission_xml():
+        result = '<?xml version="1.0" encoding="UTF-8"?>\n'
+        result += '<SUBMISSION_SET>\n\t<SUBMISSION alias=\"analysis\">\n'
+        result += '\t\t<ACTIONS>\n\t\t\t<ACTION>\n\t\t\t\t<ADD/>\n\t\t\t' \
+                  '</ACTION>\n'
+        result += '\t\t\t<ACTION>\n\t\t\t\t<RELEASE/>\n\t\t\t</ACTION>\n\t\t' \
+                  '</ACTIONS>\n'
+        result += '\t</SUBMISSION>\n</SUBMISSION_SET>\n'
+        return result
--- metadata_validation_conversion/submission/views.py
+++ metadata_validation_conversion/submission/views.py
@@ -2,7 +2,9 @@ import json
 from django.http import HttpResponse
 from metadata_validation_conversion.celery import app
 from metadata_validation_conversion.helpers import send_message
-from .tasks import prepare_samples_data, prepare_analyses_data, prepare_experiments_data
+from .tasks import prepare_samples_data, prepare_analyses_data, \
+    prepare_experiments_data
+from .helpers import zip_files
 
 
 def samples_submission(request, task_id):
@@ -36,7 +38,13 @@ def analyses_submission(request, task_id):
 def send_data(request, task_id):
     conversion_results = app.AsyncResult(task_id)
     data_to_send = conversion_results.get()
-    response = HttpResponse(json.dumps(data_to_send),
-                            content_type='application/json')
-    response['Content-Disposition'] = 'attachment; filename="samples.json"'
+    if isinstance(data_to_send, list):
+        zipped_file = zip_files(data_to_send[1:], data_to_send[0])
+        response = HttpResponse(zipped_file,
+                                content_type='application/octet-stream')
+        response['Content-Disposition'] = 'attachment; filename="analyses.zip"'
+    else:
+        response = HttpResponse(json.dumps(data_to_send),
+                                content_type='application/json')
+        response['Content-Disposition'] = 'attachment; filename="samples.json"'
     return response
