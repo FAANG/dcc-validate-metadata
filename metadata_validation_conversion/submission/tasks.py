079c23ae19002bd4280e5a33e5cc53cb0414b9c3
Alexey Sokolov <alexey@ebi.ac.uk>
2019-12-05 16:25:01 +0000

    Adding errors handling

--- metadata_validation_conversion/submission/tasks.py
+++ metadata_validation_conversion/submission/tasks.py
@@ -28,5 +28,18 @@ def prepare_experiments_data(json_to_convert):
     conversion_results = ExperimentFileConverter(json_to_convert)
     experiment_xml, run_xml, study_xml, submission_xml = \
         conversion_results.start_conversion()
-    send_message(submission_status='Data is ready')
+    if 'Error' in experiment_xml:
+        send_message(submission_status='Failed to convert data',
+                     conversion_errors=experiment_xml)
+    elif 'Error' in run_xml:
+        send_message(submission_status='Failed to convert data',
+                     conversion_errors=run_xml)
+    elif 'Error' in study_xml:
+        send_message(submission_status='Failed to convert data',
+                     conversion_errors=study_xml)
+    elif 'Error' in submission_xml:
+        send_message(submission_status='Failed to convert data',
+                     conversion_errors=submission_xml)
+    else:
+        send_message(submission_status='Data is ready')
     return 'experiment', experiment_xml, run_xml, study_xml, submission_xml
