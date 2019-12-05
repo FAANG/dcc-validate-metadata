850e160f6c5c1a97a8e5025d118bc1ed3ac44991
Alexey Sokolov <alexey@ebi.ac.uk>
2019-12-05 15:57:22 +0000

    Adding converter for experiment

--- metadata_validation_conversion/submission/helpers.py
+++ metadata_validation_conversion/submission/helpers.py
@@ -21,3 +21,27 @@ def zip_files(files, output_type):
         for n, f in enumerate(files):
             zf.writestr(file_names[n], f)
     return outfile.getvalue()
+
+
+def check_field_existence(field_to_check, record_to_check):
+    """
+    This function will check for existence of field in record
+    :param field_to_check: field to search for
+    :param record_to_check: record to search field in
+    :return:
+    """
+    if field_to_check in record_to_check:
+        return record_to_check[field_to_check]
+    else:
+        return None
+
+
+def remove_underscores(value_to_convert):
+    """
+    This function will return value without underscores
+    :param value_to_convert: value to convert
+    :return: value with spaces instead of underscores
+    """
+    return " ".join(value_to_convert.split("_"))
+
+
