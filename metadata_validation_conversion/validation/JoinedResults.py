e2ef1c5d9a89c0add015911b5f7d29673f011d56
Alexey Sokolov <alexey@ebi.ac.uk>
2020-03-15 16:11:00 +0000

    Code refactoring

--- metadata_validation_conversion/validation/JoinedResults.py
+++ metadata_validation_conversion/validation/JoinedResults.py
@@ -6,19 +6,17 @@ class JoinedResults:
         joined_results = dict()
         number_of_results = len(self.results)
         for record_type in self.results[0]:
-            if record_type == 'table':
-                continue
             joined_results.setdefault(record_type, list())
             for index, first_record in enumerate(self.results[0][record_type]):
                 records = list()
                 for i in range(0, number_of_results):
                     records.append(self.results[i][record_type][index])
-                tmp = self.join_record(*records)
+                tmp = self.join_record(records)
                 joined_results[record_type].append(tmp)
         return joined_results
 
     @staticmethod
-    def join_record(*records):
+    def join_record(records):
         """
         This function will join all issues into one result structure
         :param records: list of records to join
@@ -27,13 +25,21 @@ class JoinedResults:
         results_to_return = dict()
         for record in records:
             for name, value in record.items():
-                if name == 'samples_core' or name == 'custom' \
-                        or name == 'module':
+                if name in ['samples_core', 'custom', 'module',
+                            'experiments_core']:
                     results_to_return.setdefault(name, dict())
                     for k, v in record[name].items():
-                        # TODO: check that v is array
-                        results_to_return[name].setdefault(k, dict())
-                        results_to_return[name][k].update(v)
+                        if isinstance(v, dict):
+                            results_to_return[name].setdefault(k, dict())
+                            results_to_return[name][k].update(v)
+                        else:
+                            results_to_return[name].setdefault(k, list())
+                            for index, item in enumerate(v):
+                                try:
+                                    results_to_return[name][k][index].update(
+                                        item)
+                                except IndexError:
+                                    results_to_return[name][k].append(item)
                 else:
                     if isinstance(value, dict):
                         results_to_return.setdefault(name, dict())
