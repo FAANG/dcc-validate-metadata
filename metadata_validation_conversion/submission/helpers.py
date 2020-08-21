bcc42424b2b8ab28f70d01548e0ff7f6f24e9b29
Alexey Sokolov <alexey@ebi.ac.uk>
2020-08-21 14:06:07 +0100

    Fixing analysis conversion

--- metadata_validation_conversion/submission/helpers.py
+++ metadata_validation_conversion/submission/helpers.py
@@ -1,28 +1,3 @@
-import io
-import zipfile
-
-
-def zip_files(files, output_type):
-    """
-    This function will extract files from tuple and zip it in one archive
-    :param files: files to extract and zip
-    :param output_type: type of conversion
-    :return: zip archive
-    """
-    analysis_files = ['analysis.xml', 'submission.xml']
-    experiments_files = ['experiment.xml', 'run.xml', 'study.xml',
-                         'submission.xml']
-    if output_type == 'analysis':
-        file_names = analysis_files
-    else:
-        file_names = experiments_files
-    outfile = io.BytesIO()
-    with zipfile.ZipFile(outfile, 'w') as zf:
-        for n, f in enumerate(files):
-            zf.writestr(file_names[n], f)
-    return outfile.getvalue()
-
-
 def check_field_existence(field_to_check, record_to_check):
     """
     This function will check for existence of field in record
