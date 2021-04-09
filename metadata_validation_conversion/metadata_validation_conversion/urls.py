f207beaca5885f356caa7be144761acb0d2aaf5e
Akshatha Nayak <akshatha@ebi.ac.uk>
2021-04-09 12:41:33 +0530

    adding ontology service

--- metadata_validation_conversion/metadata_validation_conversion/urls.py
+++ metadata_validation_conversion/metadata_validation_conversion/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('submission/', include('submission.urls')),
     path('protocols_upload/', include('protocols_upload.urls')),
     path('private_portal/', include('private_portal.urls')),
+    path('ontology_improver/', include('ontology_improver.urls')),
     path('admin_panel/', admin.site.urls),
     path(r'api-token-auth/', obtain_jwt_token),
     path(r'api-token-refresh/', refresh_jwt_token)
