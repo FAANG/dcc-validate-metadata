d74367e082a63d5fcdb5c88ecc0193eb651ad0c2
Akshatha Nayak <akshatha@ebi.ac.uk>
2021-04-16 11:39:47 +0530

    ontology service db models and endpoints

--- metadata_validation_conversion/ontology_improver/models.py
+++ metadata_validation_conversion/ontology_improver/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Ontologies(models.Model):
+    ontology_term = models.TextField()
+    ontology_type = models.TextField()
+    ontology_id = models.TextField()
+    ontology_support = models.TextField()
+    ontology_status = models.TextField()
+    colour_code = models.TextField()
+    users = models.TextField()
+    last_updated = models.DateTimeField(default=datetime.now, blank=True)
--- run_daphne.sh
+++ run_daphne.sh
@@ -1,3 +1,4 @@
 python manage.py collectstatic
+python manage.py makemigrations
 python manage.py migrate
 daphne --bind 0.0.0.0 --port 8000 metadata_validation_conversion.asgi:application
