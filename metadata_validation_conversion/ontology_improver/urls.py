cd49a7e0bb3ae3a89a066647d700b60212f1775b
Akshatha Nayak <akshatha@ebi.ac.uk>
2021-06-11 12:38:22 +0530

    get ontology id as path param

--- metadata_validation_conversion/ontology_improver/urls.py
+++ metadata_validation_conversion/ontology_improver/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('search/', views.search_terms, name='search_terms'),
     path('get_matches/', views.get_zooma_ontologies, name='get_zooma_ontologies'),
     path('validate/', views.validate_terms, name='validate_terms'),
-    path('ontology_detail', views.get_ontology_details, name='ontology_detail')
+    path('ontology_detail/<str:id>', views.get_ontology_details, name='ontology_detail')
 ]
