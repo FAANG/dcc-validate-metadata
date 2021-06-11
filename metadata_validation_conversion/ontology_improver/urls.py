752d5a27e71148fe03b1e89ee73d064fdb74bea6
Akshatha Nayak <akshatha@ebi.ac.uk>
2021-06-11 12:25:01 +0530

    get ontology details from EBI OLS

--- metadata_validation_conversion/ontology_improver/urls.py
+++ metadata_validation_conversion/ontology_improver/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('auth/', views.authentication, name='authentication'),
     path('search/', views.search_terms, name='search_terms'),
     path('get_matches/', views.get_zooma_ontologies, name='get_zooma_ontologies'),
-    path('validate/', views.validate_terms, name='validate_terms')
+    path('validate/', views.validate_terms, name='validate_terms'),
+    path('ontology_detail', views.get_ontology_details, name='ontology_detail')
 ]
