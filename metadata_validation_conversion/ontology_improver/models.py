6b4e72776110b897535167a6c0ffdf23655377b4
Akshatha Nayak <akshatha@ebi.ac.uk>
2021-06-15 17:22:25 +0530

    updating API endpoints and making database schema changes

--- metadata_validation_conversion/ontology_improver/models.py
+++ metadata_validation_conversion/ontology_improver/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from datetime import datetime
 
+class User(models.Model):
+    username = models.TextField(primary_key=True)
+    first_name = models.TextField()
+    last_name = models.TextField()
+    email = models.EmailField()
+    institute = models.TextField()
+
 class Ontologies(models.Model):
     ontology_term = models.TextField()
     ontology_type = models.TextField()
@@ -8,6 +15,9 @@ class Ontologies(models.Model):
     ontology_support = models.TextField()
     ontology_status = models.TextField()
     colour_code = models.TextField()
-    users = models.TextField()
+    project = models.TextField()
+    species = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
     verified_count = models.IntegerField()
-    last_updated = models.DateTimeField(default=datetime.now, blank=True)
+    created_date = models.DateTimeField(default=datetime.now, blank=True)
+
