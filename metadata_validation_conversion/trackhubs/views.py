6d29e2a7c1aa831dd02d9def6419f39a5679b15d
Akshatha Nayak <akshatha@ebi.ac.uk>
2021-09-02 21:58:57 +0530

    tracks validations

--- metadata_validation_conversion/trackhubs/views.py
+++ metadata_validation_conversion/trackhubs/views.py
@@ -22,14 +22,13 @@ def upload_tracks(request, genome_id, directory):
 def upload_text_files(request, genome_id):
     if request.method == 'POST':
         fileid = list(request.FILES.keys())[0]
-        filename = str(request.FILES[fileid]).split("_")
+        filename = str(request.FILES[fileid])
         firepath = genome_id
         with open(f'/data/{fileid}.bb', 'wb+') as destination:
             for chunk in request.FILES[fileid].chunks():
                 destination.write(chunk)
-        validate_task = validate.s(fileid, filename).set(queue='upload')
-        upload_task = upload.s(fileid, firepath,
-                               str(request.FILES[fileid])).set(queue='upload')
+        validate_task = validate.s(fileid, filename, genome_id).set(queue='upload')
+        upload_task = upload.s(fileid, firepath, filename).set(queue='upload')
         upload_files_chain = chain(validate_task | upload_task)
         res = upload_files_chain.apply_async()
         return HttpResponse(json.dumps({"id": res.id}))
