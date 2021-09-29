605118511632e8ab9fffadea1a1a10ed2e775c67
Akshatha Nayak <akshatha@ebi.ac.uk>
2021-09-29 15:47:36 +0100

    register with trackhubs registry and associate tracks with relevant records

--- metadata_validation_conversion/trackhubs/urls.py
+++ metadata_validation_conversion/trackhubs/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'trackhubs'
 urlpatterns = [
-    path('<str:genome_id>/<str:directory>', views.upload_tracks, name='upload_tracks'),
-    path('<str:genome_id>', views.upload_text_files, name='upload_text_files'),
+    path('register/', views.register_trackhub, name='register_trackhub'),
+    path('upload/<str:genome_id>/<str:directory>', views.upload_tracks, name='upload_tracks'),
+    path('upload/<str:genome_id>', views.upload_text_files, name='upload_text_files'),
 ]
