2d368e1922011f6b8bf1fad44ce9c52054b4fb71
Alexey Sokolov <alexey@ebi.ac.uk>
2021-11-24 13:56:27 +0000

    Removing private_portal

--- private_portal/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from rest_framework import routers
-
-from . import views
-
-urlpatterns = [
-    path('<str:data_type>/', views.OrganismsView.as_view(),
-         name='private_data'),
-]
