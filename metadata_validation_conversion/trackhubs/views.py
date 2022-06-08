22eef9d6a3e42c18c6697504a35002acc9491bab
Akshatha Nayak <akshatha@ebi.ac.uk>
2022-06-08 13:35:35 +0100

    minor changes

--- metadata_validation_conversion/trackhubs/views.py
+++ metadata_validation_conversion/trackhubs/views.py
@@ -14,15 +14,15 @@ def validation(request):
             for chunk in request.FILES[fileid].chunks():
                 destination.write(chunk)
         # Convert Excel file to json 
-        read_task = read_excel_file.s(fileid).set(queue='upload')
+        read_task = read_excel_file.s(fileid).set(queue='validation')
         # Validation level #1: validate json, generate error/ warnings list
-        validate_task = validate.s(fileid).set(queue='upload')
+        validate_task = validate.s(fileid).set(queue='validation')
         # Generate hub files
-        generate_task = generate_hub_files.s(fileid).set(queue='upload')
+        generate_task = generate_hub_files.s(fileid).set(queue='validation')
         # Upload track data files and hub files to file server
-        upload_task = upload_files.s(fileid).set(queue='upload')
+        upload_task = upload_files.s(fileid).set(queue='validation')
         # Validation level #2: hubcheck, generate error/ warnings list
-        hubcheck_task = hub_check.s(fileid).set(queue='upload')
+        hubcheck_task = hub_check.s(fileid).set(queue='validation')
         validation_chain = chain(read_task | validate_task \
              | generate_task | upload_task | hubcheck_task)
         res = validation_chain.apply_async()
@@ -35,9 +35,9 @@ def submission(request):
         data = json.loads(request.body)
         roomid = data['hub_dir']
         # register trackhub with the trackhub registry
-        register_task = register_trackhub.s(data, roomid).set(queue='upload')
+        register_task = register_trackhub.s(data, roomid).set(queue='submission')
         # add track hub url to relevant specimen records
-        associate_task = associate_specimen.s(roomid).set(queue='upload')
+        associate_task = associate_specimen.s(roomid).set(queue='submission')
         submission_chain = chain(register_task | associate_task)
         res = submission_chain.apply_async()
         return HttpResponse(json.dumps({"id": res.id}))
