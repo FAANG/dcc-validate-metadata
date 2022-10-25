cd84073c8791e377f69807a645a98e4a2dd7c5af
Alexey Sokolov <alexey@ebi.ac.uk>
2022-10-25 15:03:07 +0100

    Fixing bugs

--- metadata_validation_conversion/validation/ElixirValidatorResults.py
+++ metadata_validation_conversion/validation/ElixirValidatorResults.py
@@ -95,8 +95,7 @@ class ElixirValidatorResults:
         print(paths)
         print(additional_field)
         for i, error in enumerate(errors):
-            if '.' in error:
-                print('here')
+            if '.' in paths[i]:
                 key = error.split(".")[1]
                 self.update_record_to_return(record_to_return, key, error,
                                              additional_field=additional_field)
