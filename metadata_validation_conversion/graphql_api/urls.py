24bcb13563516d3dec530097b0d0f65a952d6a58
Yogmatee Roochun <yroochun@ebi.ac.uk>
2022-12-06 09:48:19 +0000

    graph-ql backend added

--- metadata_validation_conversion/graphql_api/urls.py
+++ metadata_validation_conversion/graphql_api/urls.py
@@ -7,8 +7,7 @@ from . import views
 DEBUG = config('DEBUG', default=False)
 
 urlpatterns = [
-    #  TODO remove csrf_exempt in prod : https://stackoverflow.com/questions/51764452/403-by-graphene-django-dont-use-csrf-exempt
-    path("", csrf_exempt(GraphQLView.as_view(graphiql=DEBUG))),
+    path("", csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path("tasks/<str:task_id>", views.get_task_details)
 
 ]
