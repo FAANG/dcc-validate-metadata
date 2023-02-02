fcd178ed643dcc953d14fcbb0ef6b6d97a18fe36
Alexey Sokolov <alexey@ebi.ac.uk>
2023-02-02 13:56:28 +0000

    Updating organization constants

--- metadata_validation_conversion/metadata_validation_conversion/constants.py
+++ metadata_validation_conversion/metadata_validation_conversion/constants.py
@@ -381,6 +381,7 @@ ENA_PROD_SERVER = 'https://www.ebi.ac.uk/ena/submit/drop-box/submit/'
 
 
 ORGANIZATIONS = {
+    "ABDN": "University of Aberdeen (Aberdeen, UK)",
     "AGR": "AgResearch (New Zealand)",
     "AGS": "Agroscope (Switzerland)",
     "DEDJTR": "Department of Economic Development, Jobs, Transport and "
@@ -389,28 +390,36 @@ ORGANIZATIONS = {
     "EHU": "University of the Basque Country (Spain)",
     "ESTEAM": "ESTeam Paris SUD (France)",
     "FBN": "Leibniz Institute for Farm Animal Biology (Dummerstorf, Germany)",
+    "HCMR": "Hellenic Centre for Marine Research (Greece)",
     "INRA": "French National Institute for Agricultural Research (France)",
     "INRAE": "National Research Institute for Agriculture, Food and "
              "Environment (France）",
-   "INSERM": "French National Institute of Health and Medical Research "
-              "(France)",
+    "INSERM": "French National Institute of Health and Medical Research (France)",
     "INSERM-INRAE": "INSERM-INRAE",
+    "IRTA": "Institute of Agrifood Research and Technology (Spain)",
     "ISU": "Iowa State University (USA)",
     "KU": "Konkuk University (Seoul, Korea)",
     "NUID": "University College Dublin (Dublin, Ireland)",
     "NMBU": "Norwegian University of Life Sciences (Norway)",
+    "QAAFI-UQ": "University of Queensland (Australia)",
     "ROSLIN": "Roslin Institute (Edinburgh, UK)",
     "TAMU": "Texas A&M University (USA)",
+    "UAL": "University of Alberta (Canada)",
     "UCD": "University of California, Davis (USA)",
-    "UIC": "University of Illinois at Chicago (USA)",
-    "UIUC": "University of Illinois at Urbana–Champaign (USA)",
     "UD": "University of Delaware (USA)",
     "UDL": "University of Lleida (Catalonia, Spain)",
+    "UIC": "University of Illinois at Chicago (USA)",
+    "UIDAHO": "University of Idaho (USA)",
+    "UIUC": "University of Illinois at Urbana–Champaign (USA)",
     "ULE": "University of León (León, Spain)",
+    "UNIPD": "University of Padua (Italy)",
+    "UNL": "University of Nebraska-Lincoln (USA)",
+    "UOB": "University of Birmingham (UK)",
+    "USC": "University of Santiago de Compostela (Spain)",
     "USDA": "The United States Department of Agriculture (USA)",
     "USMARC": "United States Meat Animal Research Center (USA)",
-    "UAL": "University of Alberta (Canada)",
-    "UIDAHO": "University of Idaho (USA)",
     "USU": "Utah State University (USA)",
-    "WSU": "Washington State University(USA)"
+    "WSU": "Washington State University(USA)",
+    "WU": "Wageningen University (Netherlands)",
+    "ZIGR": "Polish Academy of Sciences Institute of Ichthyobiology and Aquaculture in Golysz (Poland)"
 }
