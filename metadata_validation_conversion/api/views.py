46865cd234b22baa81cd9e87f438b7d76df684cd
Yogmatee Roochun <yroochun@ebi.ac.uk>
2023-03-08 11:26:18 +0000

    added submissions_test index

--- metadata_validation_conversion/api/views.py
+++ metadata_validation_conversion/api/views.py
@@ -24,7 +24,8 @@ ALLOWED_INDICES = ['file', 'organism', 'specimen', 'dataset', 'experiment',
                    'protocol_files', 'protocol_samples', 'article',
                    'protocol_analysis', 'analysis', 'summary_organism',
                    'summary_specimen', 'summary_dataset', 'summary_file',
-                   'ensembl_annotation', 'trackhubs', 'submissions']
+                   'ensembl_annotation', 'trackhubs', 'submissions',
+                   'submissions_test']
 
 @swagger_auto_schema(method='get', tags=['Search'],
         operation_summary="Get a list of Organisms, Specimens, Files, Datasets etc",
