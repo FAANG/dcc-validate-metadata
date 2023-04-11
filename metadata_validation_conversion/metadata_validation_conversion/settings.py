42fbff9e40569dede493f1fc02be707e7b04c3e3
Yogmatee Roochun <yroochun@ebi.ac.uk>
2023-04-11 11:36:59 +0100

    updated email_host secrets

--- metadata_validation_conversion/metadata_validation_conversion/settings.py
+++ metadata_validation_conversion/metadata_validation_conversion/settings.py
@@ -220,9 +220,8 @@ SWAGGER_SETTINGS = {
     'TAGS_SORTER': 'alpha'
 }
 
-
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'outgoing.ebi.ac.uk'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'xxx@ebi.ac.uk'
-EMAIL_HOST_PASSWORD = 'xxx'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USERNAME')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
