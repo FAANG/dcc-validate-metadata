5e519a90f0d37c0ebaa2c598f879feed9c8cef74
Mil-m <mansurova.m94@gmail.com>
2024-01-04 11:23:18 +0000

    abc-262 redis-svc in RedisChannelLayer

--- metadata_validation_conversion/metadata_validation_conversion/settings.py
+++ metadata_validation_conversion/metadata_validation_conversion/settings.py
@@ -171,7 +171,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(os.getenv('REDIS_HOST'), 6379)],
+            "hosts": [('redis-svc', 6379)],
         },
     },
 }
