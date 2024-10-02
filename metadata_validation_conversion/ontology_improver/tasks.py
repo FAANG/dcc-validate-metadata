b0297e90302588490d3e029ec39e5f7ee6513390
Alexey Sokolov <alexey@k7cw6gw9kg.windows.ebi.ac.uk>
2024-10-02 09:32:53 +0100

    Disabling pandas

--- metadata_validation_conversion/ontology_improver/tasks.py
+++ metadata_validation_conversion/ontology_improver/tasks.py
@@ -1,6 +1,6 @@
 import json
 import requests
-import pandas as pd
+# import pandas as pd
 from collections import Counter
 from django.conf import settings
 from elasticsearch import Elasticsearch, RequestsHttpConnection
@@ -45,40 +45,40 @@ def get_species_for_project(project):
 
 @app.task
 def update_ontology_summary():
-    url = f'{BE_SVC}/data/ontologies/_search/?size=10000'
-    resultset = requests.get(url).json()['hits']['hits']
-    ontologies = map(lambda ontology: ontology['_source'], resultset)
-    df = pd.DataFrame.from_dict(ontologies)[['projects', 'type', 'term']]
-    df['type'] = [', '.join(map(str, l)) for l in df['type']]
-    df = df.explode('projects')
-    df = df.loc[df['projects'].isin(PROJECTS)]
-    df = df.groupby(['projects']).agg({
-        'type': comma_separated_combine,
-        'term': 'count'
-    }).reset_index()
-    df['type'] = df['type'].apply(type_count)
-    # get project-specific species from organisms index
-    species = {}
-    for project in PROJECTS:
-        if project in df['projects'].values:
-            species[get_species_for_project(project)] = project
-    df['species'] = species
-    # get existing summary statistics
-    url = f"{BE_SVC}/data/summary_ontologies/_search/?size=10"
-    res_data = requests.get(url).json()['hits']['hits']
-    validated_counts = {}
-    for record in res_data:
-        validated_counts[record['_id']] = record['_source']['activity']['validated_count']
-    # generate update payload
-    for index, row in df.iterrows():
-        updated_project_stats = {
-            'project': row['projects'],
-            'species': row['species'],
-            'type_counts': convertToListOfDict(row['type']),
-            'activity': {
-                'created_edited_count': row['term'],
-                'validated_count': validated_counts[row['projects']] if row['projects'] in validated_counts else 0
-            }
-        }
-        es.index(index='summary_ontologies', id=updated_project_stats['project'], body=updated_project_stats)
+    # url = f'{BE_SVC}/data/ontologies/_search/?size=10000'
+    # resultset = requests.get(url).json()['hits']['hits']
+    # ontologies = map(lambda ontology: ontology['_source'], resultset)
+    # df = pd.DataFrame.from_dict(ontologies)[['projects', 'type', 'term']]
+    # df['type'] = [', '.join(map(str, l)) for l in df['type']]
+    # df = df.explode('projects')
+    # df = df.loc[df['projects'].isin(PROJECTS)]
+    # df = df.groupby(['projects']).agg({
+    #     'type': comma_separated_combine,
+    #     'term': 'count'
+    # }).reset_index()
+    # df['type'] = df['type'].apply(type_count)
+    # # get project-specific species from organisms index
+    # species = {}
+    # for project in PROJECTS:
+    #     if project in df['projects'].values:
+    #         species[get_species_for_project(project)] = project
+    # df['species'] = species
+    # # get existing summary statistics
+    # url = f"{BE_SVC}/data/summary_ontologies/_search/?size=10"
+    # res_data = requests.get(url).json()['hits']['hits']
+    # validated_counts = {}
+    # for record in res_data:
+    #     validated_counts[record['_id']] = record['_source']['activity']['validated_count']
+    # # generate update payload
+    # for index, row in df.iterrows():
+    #     updated_project_stats = {
+    #         'project': row['projects'],
+    #         'species': row['species'],
+    #         'type_counts': convertToListOfDict(row['type']),
+    #         'activity': {
+    #             'created_edited_count': row['term'],
+    #             'validated_count': validated_counts[row['projects']] if row['projects'] in validated_counts else 0
+    #         }
+    #     }
+    #     es.index(index='summary_ontologies', id=updated_project_stats['project'], body=updated_project_stats)
     return "Success"
